62823b8585b8f73d235bd566c5150f16052df350
Mark Kelly <markk0042@gmail.com>
2022-08-19 09:06:59 +0000

    updated tabs for information about me and also my work timeline

--- css/assets/js/sendEmail.js
+++ css/assets/js/sendEmail.js
@@ -1,3 +1,19 @@
-console.log('hello')
+
+        function sendMail(contactForm) {
+    emailjs.send("service_uhn0qpl", "rosie", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+};
 
 
